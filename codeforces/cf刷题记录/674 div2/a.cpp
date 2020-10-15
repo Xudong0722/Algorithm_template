074327d91b06e251c9b3a2204c341fffc0c901f7
Xudong_Zhai <49638560+Xudong0722@users.noreply.github.com>
2020-10-15 17:25:30 +0800

    Update a.cpp

--- codeforces/cf刷题记录/674 div2/a.cpp
+++ codeforces/cf刷题记录/674 div2/a.cpp
@@ -1,1 +1,75 @@
+/*
+  简单的数学题，除一下即可。
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define lowbit(x) ((x)&(-x))
+#define sz(x) ((int)x.size())
+#define fr(x) freopen(x,'r',stdin)
+#define fw(x) freopen(x,'w',stdout)
+#define mst(x,a) memset(x,a,sizeof(x))
+#define all(a) begin(a),end(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define rep(i,a,b) for(int i = (a);i<=(b); ++i)
+#define per(i,a,b) for(int i = (b);i>=(a); --i)
+typedef long long LL;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+typedef pair<double,double> PDD;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<double> VB;
+typedef double db;
 
+template <class T> void clear(T& a) { T().swap(a);}
+template <class T,class S>
+inline bool upmin(T& a,const S&b){ return a>b ? a=b,1:0;}
+template <class T,class S>
+inline bool upmax(T&a, const S&b){ return a<b? a=b,1:0;}
+//global variable
+const int N = 1;
+const int mod = 1e9+7;
+int t,n,x;
+
+
+//functions
+LL qmi(LL a,LL k,LL mod){
+    LL res = 1;
+    while(k){
+        if(k&1) res = res*a%mod;
+        k >>= 1;
+        a = a*a%mod;
+    }
+    return mod;
+}
+int gcd(int a,int b) {
+    return b==0? a:gcd(b,a%b);
+}
+//
+
+
+//code from here! Come on! Have a pleasant experience~
+int main()
+{
+    IO;
+    cin>>t;
+    while(t--){
+        cin>>n>>x;
+
+        int ans = 1;
+        if(n<=2) cout<<ans<<endl;
+        else{
+            int t = (n-2)/x;
+            int left = (n-2)%x;
+            if(t) ans += t;
+            if(left) ans ++;
+            cout<<ans<<endl;
+        }
+    }
+    return 0;
+}
