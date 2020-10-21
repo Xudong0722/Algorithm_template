2e235f073c229800936db235d4581dfcc2e5313a
Xudong_Zhai <49638560+Xudong0722@users.noreply.github.com>
2020-10-22 00:33:03 +0800

    Update a.cpp

--- codeforces/cf刷题记录/677 div3/a.cpp
+++ codeforces/cf刷题记录/677 div3/a.cpp
@@ -1,1 +1,116 @@
 
+/*
+ * Author: Moon-light
+ * Time: 2020-10-21 23:46:07
+  先预处理出所有符合条件的数，再循环一边即可
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define lowbit(x) ((x)&(-x))
+#define sz(x) ((int)x.size())
+#define fr(x) freopen(x,'r',stdin)
+#define fw(x) freopen(x,'w',stdout)
+#define mst(x,a) memset(x,a,sizeof(x))
+#define all(a) begin(a),end(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define rep(i,a,b) for(int i = (a);i<=(b); ++i)
+#define per(i,a,b) for(int i = (a);i>=(b); --i)
+typedef long long LL;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+typedef pair<double,double> PDD;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<double> VB;
+typedef double db;
+
+template <class T> void clear(T& a) { T().swap(a);}
+template <class T,class S>
+inline bool upmin(T& a,const S&b){ return a>b ? a=b,1:0;}
+template <class T,class S>
+inline bool upmax(T&a, const S&b){ return a<b? a=b,1:0;}
+
+//fast read(improved by fread)
+char buf[1<<21],*p1=buf,*p2=buf;
+inline int getc(){
+    return p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++;
+}
+inline int redi() {
+    int ret = 0,f = 0;char ch = getc();
+    while (!isdigit (ch)) {
+        if (ch == '-') f = 1;
+        ch = getc();
+    }
+    while (isdigit (ch)) {
+        ret = ret * 10 + ch - 48;
+        ch = getc();
+    }
+    return f?-ret:ret;
+}
+
+//global variable
+const int N = 1000;
+const int mod = 1e9+7;
+int t,x;
+int a[N],b[N];
+
+
+
+//functions
+LL qmi(LL a,LL k,LL mod){
+    LL res = 1;
+    while(k){
+        if(k&1) res = res*a%mod;
+        k >>= 1;
+        a = a*a%mod;
+    }
+    return mod;
+}
+int gcd(int a,int b) {
+    return b==0? a:gcd(b,a%b);
+}
+//
+
+
+//code from here! Come on! Have a pleasant experience~
+int main()
+{
+    IO;
+    cin>>t;
+    
+    int res,cnt = 0;
+    for(int i = 1; i<=9; i++){
+        int t = i;
+        res = 0;
+        while(1){
+            if(i*res+t>9999) break;
+            a[++cnt] = i*res+t;
+            t = a[cnt];
+            
+            if(!res) {
+                res = 10;
+                b[cnt] = 1;
+            }
+            else {
+                res *= 10;
+                b[cnt] = log10(res);
+            }
+        }
+    }
+
+    while(t--){
+        cin>>x;
+        int ans = 0;
+        rep(i,1,cnt){
+            ans += b[i];
+            if(a[i] == x) break;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
