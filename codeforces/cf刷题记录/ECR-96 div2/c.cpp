bb6a140be18d7dab8021a5609adb2b7f0310cfe1
Xudong_Zhai <49638560+Xudong0722@users.noreply.github.com>
2020-10-27 22:12:45 +0800

    Create c.cpp

---
+++ codeforces/cf刷题记录/ECR-96 div2/c.cpp
@@ -0,0 +1,105 @@
+/*
+ * Author: Moon-light
+ * Time: 2020-10-27 16:45:31
+ * 推了一遍放在，只要从后往前推，最后的答案总是2
+ * 因为每次取两个数一半，新的数肯定小于等于这两个数，即越来越小
+ * 可用优先队列大根堆来模拟这个过程
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define lowbit(x) ((x)&(-x))
+#define sz(x) ((int)x.size())
+#define fr(x) freopen(x,'r',stdin)
+#define fw(x) freopen(x,'w',stdout)
+#define mst(x,a) memset(x,a,sizeof(x))
+#define all(a) begin(a),end(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define rep(i,a,b) for(int i = (a);i<=(b); ++i)
+#define per(i,a,b) for(int i = (a);i>=(b); --i)
+typedef long long LL;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+typedef pair<double,double> PDD;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<double> VB;
+typedef double db;
+
+template <class T> void clear(T& a) { T().swap(a);}
+template <class T,class S>
+inline bool upmin(T& a,const S&b){ return a>b ? a=b,1:0;}
+template <class T,class S>
+inline bool upmax(T&a, const S&b){ return a<b? a=b,1:0;}
+
+//fast read(improved by fread)
+char buf[1<<21],*p1=buf,*p2=buf;
+inline int getc(){
+    return p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++;
+}
+inline int redi() {
+    int ret = 0,f = 0;char ch = getc();
+    while (!isdigit (ch)) {
+        if (ch == '-') f = 1;
+        ch = getc();
+    }
+    while (isdigit (ch)) {
+        ret = ret * 10 + ch - 48;
+        ch = getc();
+    }
+    return f?-ret:ret;
+}
+
+//global variable
+const int N = 2e5+100;
+const int mod = 1e9+7;
+int t,n;
+
+
+//functions
+int  myround(int x,int y){
+    return (x-1)/y+1;
+}
+LL qmi(LL a,LL k,LL mod){
+    LL res = 1;
+    while(k){
+        if(k&1) res = res*a%mod;
+        k >>= 1;
+        a = a*a%mod;
+    }
+    return mod;
+}
+int gcd(int a,int b) {
+    return b==0? a:gcd(b,a%b);
+}
+//
+
+
+//code from here! Come on! Have a pleasant experience~
+int main()
+{
+    IO;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        priority_queue<int,VI,less<int>> h;
+
+        rep(i,1,n){
+            h.push(i);
+        }
+
+        int m = n-1;
+        cout<<2<<endl;
+        while(m--){
+            int a = h.top(); h.pop();
+            int b = h.top(); h.pop();
+            cout<<a<<" "<<b<<endl;
+            h.push(myround(a+b,2));
+        }
+    }
+    return 0;
+}
