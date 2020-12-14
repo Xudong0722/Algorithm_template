a4391069b81a794ed1a3e5908239fac251a39eed
Xudong_Zhai <49638560+Xudong0722@users.noreply.github.com>
2020-12-14 21:37:00 +0800

    Create c.cpp

---
+++ Atcoder/ABC/ABC_185/c.cpp
@@ -0,0 +1,93 @@
+/*
+ * Author: Moon-light
+ * 题意：一根木棍分成12段的切法数量，两种切法不同当且仅当切割的位置不同
+ * 求组合数即可，注意一个木棍可切割的位置是n-1
+ * 那么answer = C(11,n-1);
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define lowbit(x) ((x)&(-x))
+#define sz(x) ((int)x.size())
+#define fr(x) freopen(x,'r',stdin)
+#define fw(x) freopen(x,'w',stdout)
+#define mst(x,a) memset(x,a,sizeof(x))
+#define all(a) begin(a),end(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define rep(i,a,b) for(int i = (a);i<=(b); ++i)
+#define per(i,a,b) for(int i = (a);i>=(b); --i)
+typedef long long LL;
+typedef pair<int,int> PII;  
+typedef pair<LL,LL> PLL;
+typedef pair<double,double> PDD;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<double> VB;
+typedef double db;
+
+template <class T> void clear(T& a) { T().swap(a);}
+template <class T,class S>
+inline bool upmin(T& a,const S&b){ return a>b ? a=b,1:0;}
+template <class T,class S>
+inline bool upmax(T&a, const S&b){ return a<b? a=b,1:0;}
+
+//fast read(improved by fread)
+char buf[1<<21],*p1=buf,*p2=buf;
+inline int getc(){
+    return p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++;
+}
+inline int redi() {
+    int ret = 0,f = 0;char ch = getc();
+    while (!isdigit (ch)) {
+        if (ch == '-') f = 1;
+        ch = getc();
+    }
+    while (isdigit (ch)) {
+        ret = ret * 10 + ch - 48;
+        ch = getc();
+    }
+    return f?-ret:ret;
+}
+
+//global variable
+const int N = 300;
+const int mod = 1e9+7;
+LL c[N][N];
+int n;
+//functions
+LL qmi(LL a,LL k,LL mod){
+    LL res = 1;
+    while(k){
+        if(k&1) res = res*a%mod;
+        k >>= 1;
+        a = a*a%mod;
+    }
+    return mod;
+}
+int gcd(int a,int b) {
+    return b==0? a:gcd(b,a%b);
+}
+
+void solve(){
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<=i; j++){
+            if(!j) c[i][j] = 1;
+            else c[i][j] = c[i-1][j] + c[i-1][j-1];
+        }
+    }
+}
+//
+
+//code from here! Come on! Have a pleasant experience~
+int main()
+{
+    IO;
+    cin>>n;
+    solve();
+    cout<<c[n-1][11]<<endl;
+    return 0;
+}
