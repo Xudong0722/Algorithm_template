af102c8037195fb3a999aa2f441df63810db7ba5
Xudong_Zhai <49638560+Xudong0722@users.noreply.github.com>
2021-01-21 14:00:10 +0800

    Create a.cpp

---
+++ Atcoder/ABC/KPC2021/a.cpp
@@ -0,0 +1,101 @@
+/*
+ * Author: Moon-light
+ * 找出C[n] 与 C[n-1] 的关系
+ * C[n] = max(C[n-1], max(a[1],a[2],...,a[n])*b[n])
+ * 这样只需要维护 1~n中最大的a[i] 即可
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define lowbit(x) ((x)&(-x))
+#define sz(x) ((int)x.size())
+#define fr(x) freopen(x,'r',stdin)
+#define fw(x) freopen(x,'w',stdout)
+#define mst(x,a) memset(x,a,sizeof(x))
+#define all(a) begin(a),end(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define rep(i,a,b) for(int i = (a);i<=(b); ++i)
+#define per(i,a,b) for(int i = (a);i>=(b); --i)
+typedef long long LL;
+typedef pair<int,int> PII;  
+typedef pair<LL,LL> PLL;
+typedef pair<double,double> PDD;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<double> VB;
+typedef double db;
+
+template <class T> void clear(T& a) { T().swap(a);}
+template <class T,class S>
+inline bool upmin(T& a,const S&b){ return a>b ? a=b,1:0;}
+template <class T,class S>
+inline bool upmax(T&a, const S&b){ return a<b? a=b,1:0;}
+
+//fast read(improved by fread)
+char buf[1<<21],*p1=buf,*p2=buf;
+inline int getc(){
+    return p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++;
+}
+inline int redi() {
+    int ret = 0,f = 0;char ch = getc();
+    while (!isdigit (ch)) {
+        if (ch == '-') f = 1;
+        ch = getc();
+    }
+    while (isdigit (ch)) {
+        ret = ret * 10 + ch - 48;
+        ch = getc();
+    }
+    return f?-ret:ret;
+}
+
+//global variable
+const int N = 2e5+100;
+const int mod = 1e9+7;
+
+
+int n;
+int a[N],b[N];
+LL c[N];
+
+//functions
+LL qmi(LL a,LL k,LL mod){
+    LL res = 1;
+    while(k){
+        if(k&1) res = res*a%mod;
+        k >>= 1;
+        a = a*a%mod;
+    }
+    return res;
+}
+int gcd(int a,int b) {
+    return b==0? a:gcd(b,a%b);
+}
+//
+
+
+//code from here! Come on! Have a pleasant experience~
+int main()
+{
+    IO;
+
+    cin>>n;
+    rep(i,1,n) cin>>a[i];
+    rep(i,1,n) cin>>b[i];
+
+    c[1] = (LL)a[1]*b[1];
+    LL res = a[1];
+
+    rep(i,2,n){
+        res = max(res,(LL)a[i]);
+        c[i] = max(c[i-1],res*b[i]);
+    }
+    rep(i,1,n){
+        cout<<c[i]<<endl;
+    }
+    return 0;
+}
